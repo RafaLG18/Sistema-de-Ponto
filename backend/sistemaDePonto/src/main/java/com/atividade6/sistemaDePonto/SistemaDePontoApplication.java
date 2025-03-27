a7e67011aeb09f100cfdd61610c916f7263137e1
Rafael Leal Guimarães <rafaellealg18@gmail.com>
2025-03-27 09:22:23 -0300

    #1: adicionando arquivos de backend

---
+++ backend/sistemaDePonto/src/main/java/com/atividade6/sistemaDePonto/SistemaDePontoApplication.java
@@ -0,0 +1,13 @@
+package com.atividade6.sistemaDePonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePontoApplication.class, args);
+	}
+
+}
---
+++ backend/sistemaDePonto/src/test/java/com/atividade6/sistemaDePonto/SistemaDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade6.sistemaDePonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
